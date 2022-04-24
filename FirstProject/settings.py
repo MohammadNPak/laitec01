ea64080ccebcc6182ba21778e36f85e02a21a4d5
NimaHeydarzadeh <nima.nami.hdrz@gmail.com>
2022-04-24 16:35:42 +0430

    first project

--- FirstProject/settings.py
+++ FirstProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5qa02#ejzuy02ls+9x1m3$m2rskmjkh6d^*qusxhtn!t7-&9ky'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
